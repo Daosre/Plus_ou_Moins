0183fa47614e0c36ded84fff9d87a6a0d2c84442
Antoine <204738276+anT01ne42@users.noreply.github.com>
2025-03-31 15:54:35 +0200

    init files

---
+++ index.js
@@ -0,0 +1,19 @@
+//À la fin du jeu, proposer : "Souhaites-tu rejouer ? (oui/non)"
+
+//Définir une fourchette personnalisée
+//Demander à l’utilisateur de choisir une borne minimale et une borne maximale en début de partie
+
+//Gérer les cas invalides
+//Si l’utilisateur entre autre chose qu’un nombre, afficher une alerte : "Ce n'est pas un nombre valide ! Essaie encore."
+
+//Mode difficile
+//Limiter le nombre d’essais à 10 maximum.
+
+//Si le joueur perd : "Dommage ! Le nombre mystère était 42."
+
+// 1. Générer un nombre aléatoire
+// 2. Initialiser un compteur de tentatives
+// 3. Boucle tant que le nombre n’est pas trouvé
+// 4. Comparer et afficher le bon message
+// 5. Afficher le nombre total de tentatives
+// 6. Bonus : proposer de rejouer
